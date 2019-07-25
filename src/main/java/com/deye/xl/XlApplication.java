0688d0f31c36ab752adec8dfdd8f06d2e6b3ba4a
xiaopen73312 <262749973@qq.com>
2019-07-25 13:37:18 +0800

    ADD

---
+++ src/main/java/com/deye/xl/XlApplication.java
@@ -0,0 +1,13 @@
+package com.deye.xl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XlApplication.class, args);
+    }
+
+}
