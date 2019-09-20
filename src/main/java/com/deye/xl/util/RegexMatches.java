49130a49dda989e81a44595f2001df6be160597f
xiaopen73312 <262749973@qq.com>
2019-09-20 16:16:31 +0800

    优化拆包

--- src/main/java/com/deye/xl/util/RegexMatches.java
+++ src/main/java/com/deye/xl/util/RegexMatches.java
@@ -12,11 +12,31 @@ public class RegexMatches {
     public static boolean regexMes(String str) {
 
         String pattern = "^&+.+!$";
-
+        int tnum = getSubStr(str, "&");
+        int endnum = getSubStr(str, "!");
+        if (tnum != endnum) {
+            return false;
+        }
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(str);
 //        System.out.println(m.matches());
         return m.matches();
     }
 
+    /**
+     * 获取字符串出现的个数
+     *
+     * @param str 传进来的原字符串
+     * @param chs 传进来的要查找的字符串
+     * @return 统计字符串出现的个数
+     */
+    public static int getSubStr(String str, String chs) {
+        // 用空字符串替换所有要查找的字符串
+        String destStr = str.replaceAll(chs, "");
+//        System.out.println(destStr);
+        // 查找字符出现的个数 = （原字符串长度 - 替换后的字符串长度）/要查找的字符串长度
+        int charCount = (str.length() - destStr.length()) / chs.length();
+
+        return charCount;
+    }
 }
